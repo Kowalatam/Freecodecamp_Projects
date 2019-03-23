9183e6a2b04d6ef4482652ad4f4fb7254dc1372f
Elizabeth Paul <elizapaul01@gmail.com>
2019-03-23 20:29:29 +0100

    update getobject()
    Updated getObject, so windows.onload works now finally right.
    
    Also changed id names so it passes freecodecamp tests.

--- index.js
+++ index.js
@@ -1,44 +1,32 @@
-const newQuote = document.querySelector("#new_quote");
-const quote = document.querySelector("#quote_text");
-const author = document.querySelector("#quote_author");
-const twitter = document.querySelector("#twitter");
+const newQuote = document.querySelector("#new-quote");
+const quote = document.querySelector("#text");
+const author = document.querySelector("#author");
+const twitter = document.querySelector("#tweet-quote");
 
-let quoteObject = {
-		quoteText: "As we express our gratitude, we must never forget action is not to utter words, but to live by them.", 
-     	quoteAuthor: "John F. Kennedy"
-    };
+let quoteObject;
 
 //add quote when page loads
-/*window.onload = function() {
-  getQuote();
-};*/
-
-//change quote after a number of seconds
-$(document).ready(function() {
-  setTimeout(function() {
-    getQuote();
-  }, 1500);
-});
+window.onload = getQuote;
 
 //change quote when button is clicked
 newQuote.addEventListener("click", getQuote);
 
-
 //function to get and assign quote and author
 function getQuote(){
-	//fetch the data
+  //fetch the data
     fetch('https://quota.glitch.me/random')
     .then(res => res.json())//response type
-    .then(data => quoteObject = data) // assign data to quoteObject
-
-   // quoteObject contains an object like the one below
-     /*{quoteText: "As we express our gratitude, we must never forget …ation is not to utter words, but to live by them.", 
-     	quoteAuthor: "John F. Kennedy"}*/
-
-	//display quote
-	quote.textContent = quoteObject["quoteText"];
-	//display author
-	author.innerHTML = "...<i class='fas fa-pencil-alt'></i> " + quoteObject["quoteAuthor"];
-	//share the quote on twitter	
-	twitter.setAttribute("href", "https://twitter.com/intent/tweet?text=" + quoteObject["quoteText"] + " - " + quoteObject["quoteAuthor"]);	
+    .then(data => {
+      quoteObject = data;   // assign data to quoteObject
+      //display quote
+      quote.textContent = quoteObject["quoteText"];
+      //display author
+      author.innerHTML = `...<i class='fas fa-pencil-alt'></i> ${quoteObject['quoteAuthor']}`;
+      //share the quote on twitter  
+      twitter.setAttribute("href", `https://twitter.com/intent/tweet?text= ${quoteObject['quoteText']} - ${quoteObject['quoteAuthor']}`); 
+    }); 
 }
+
+// data contains an object like the one below
+     /*{quoteText: "As we express our gratitude, we must never forget …ation is not to utter words, but to live by them.", 
+      quoteAuthor: "John F. Kennedy"}*/
